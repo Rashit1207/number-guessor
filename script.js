866b079fcce71ebf6869ccec48205de281387106
Rashit Giniatullin <rashitka123456789@mail.ru>
2023-01-31 20:30:09 +0300

    Закончили проект

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const min = 4, max = 10,
     inputNumber = document.querySelector('#number'),
     button = document.querySelector('#btn');
 let lives = 3,
+gameOver = false,
 message = document.querySelector('#text');
 document.querySelector('#min').textContent = min;
 document.querySelector('#max').textContent = max;
@@ -13,21 +14,33 @@ const showMessage = (text,style) => {
   message.classList.add(style);
 
 }
-
+const playAgain = (clazz) => {
+  inputNumber.disabled = true;
+  btn.textContent = 'PLAY AGAIN';
+  inputNumber.classList.add(clazz);
+}
 
 button.addEventListener('click', ()=>{
-  if (inputNumber.value == '') {
-    showMessage('You typed empty value', 'danger');
-  } else if (inputNumber.value < min || inputNumber.value > max) {
-    showMessage(`Input a number between ${min} and ${max}`, 'danger');
-  } else if (inputNumber.value == randomNum) {
-    inputNumber.disabled = true;
-    btn.textContent = 'PLAY AGAIN';
-    showMessage(`You win! the winning number is ${randomNum}`, 'success');
-  } else if (lives != 0 && inputNumber.value !== randomNum) {
-    lives--;
-    showMessage(`${inputNumber.value} is not correct! You have ${lives} guesses`, 'danger');
+  if (!gameOver){
+    if (inputNumber.value == '') {
+      showMessage('You typed empty value', 'danger');
+    } else if (inputNumber.value < min || inputNumber.value > max) {
+      showMessage(`Input a number between ${min} and ${max}`, 'danger');
+    } else if (inputNumber.value == randomNum) {
+      playAgain('win');
+      showMessage(`You win! the winning number is ${randomNum}`, 'success');
+      gameOver = true;
+    } else if (lives > 1 && inputNumber.value !== randomNum) {
+      lives--;
+      showMessage(`${inputNumber.value} is not correct! You have ${lives} guesses`, 'danger');
+    } else if(lives == 1) {
+      lives--;
+      playAgain('lose');
+      showMessage(`You lose. The correct answer was ${randomNum}`,'danger');
+      gameOver = true;
+    }
+  } else {
+    window.location.reload();
   }
-
 })
 // `Input a number between ${min} and ${max}`
